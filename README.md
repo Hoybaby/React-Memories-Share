936411251fcc5ab0f46b5c44d49151ca4c96ef8a
Hoybaby <mbartek436@gmail.com>
2022-08-16 14:38:00 -0400

    doing the intial setup on this application with installing some basic pacakgte,s installing react, etc etc. very simple stuff so far. left some comments

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+
 # React-Memories-Share
+
+A brief description of what this project does and who it's for
+
+
+## Process/Installation
+## Roadmap
+
+- Additional browser support
+
+- Add more integrations
+
+
+## Installation
---
+++ server/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+// before i had to import like const express = require('express');
+// in new version of node, i dont have to import it like this anymore
