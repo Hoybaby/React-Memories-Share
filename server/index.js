7b76639f641487a14238421408fba67a7a104f08
Hoybaby <mbartek436@gmail.com>
2022-09-18 21:25:22 -0400

    set up my database connection with Mongoose. I had an error using the mongoose set and commented it out and it worked just fine

--- server/index.js
+++ server/index.js
@@ -15,4 +15,12 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
-console.log(process.env.TEST);
+// console.log(process.env.TEST);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true})
+    .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err)=> console.log(err.message))
+
+// mongoose.set('useFindAndModify', false);
