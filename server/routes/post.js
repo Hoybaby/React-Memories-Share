bd13236a467d22f35627c371ac5b6482190781a6
Hoybaby <mbartek436@gmail.com>
2022-09-19 10:43:41 -0400

    reworked the logic in the post.js for routes. I madea controller to handle a succesful message to endpoint if it connects. this makes the routes cleaner because I only want routes to handle the routing, not really the logic

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,10 @@
 import express from 'express';
+import {getPosts} from '../controllers/posts.js'
 
 const router = express.Router();
 
 // the first part is the location/url endpoint which will call a function
 // this wont be called with localhost:5000/, because i changed how it is called in index.js
-router.get('/', (req,res) => {
-    res.send('this works');
-})
+router.get('/', getPosts);
 
 export default router;
