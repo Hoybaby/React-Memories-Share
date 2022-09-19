8ef62dc8c84896d0c61bc1c9d261cebd0bb788d1
Hoybaby <mbartek436@gmail.com>
2022-09-18 21:15:25 -0400

    adding a env and package to install a dotenv to use a enviromental variable to hide my mongodb conenction

--- server/index.js
+++ server/index.js
@@ -2,5 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 // before i had to import like const express = require('express');
 // in new version of node, i dont have to import it like this anymore
+
+const app = express();
+
+// applying a limit to make a max size for images
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+// setting up boddy parses
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+console.log(process.env.TEST);
