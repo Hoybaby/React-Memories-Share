cfcd3fa8612dbba24248031195caa5d51aa4464a
Hoybaby <mbartek436@gmail.com>
2022-09-22 15:17:24 -0400

    tweaking the back end code to make sure everything works

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -45,7 +45,7 @@ export const updatePost = async(req,res) => {
     //have to check if the ID is valid. just like i would and throw and error in java
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id,{...post, _id}, {new: true});
 
     res.json(updatedPost);
 }
