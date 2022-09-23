1ac8c18b8705bc6390cffc2d416b069a44dafbc8
Hoybaby <mbartek436@gmail.com>
2022-09-23 15:07:31 -0400

    working on the liking functionality which is working properly

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -7,8 +7,13 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
 
+import { useDispatch } from 'react-redux';
+import { deletePost, likePost } from '../../../actions/posts';
+
 // destructured the props below
 const Post = ({post, setCurrentId}) => {
+    const dispatch = useDispatch();
+
     const classes = useStyles();
     return(
         <Card className={classes.card}>
@@ -31,12 +36,12 @@ const Post = ({post, setCurrentId}) => {
                 <Typography  variant="h5"  gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={()=>{}}>
+                <Button size="small" color="primary" onClick={()=>dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small"/>
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={()=>{}}>
+                <Button size="small" color="primary" onClick={()=>dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small"/>
                     Delete
                     
