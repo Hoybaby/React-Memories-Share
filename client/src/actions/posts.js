2ba6e7065a546e9644cbd865600b10054139180b
Hoybaby <mbartek436@gmail.com>
2022-10-04 14:09:42 -0400

    made two files to makes to make variables to handle the payload which will help earase the erros I will make with typos

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, UPDATE, DELETE, CREATE } from '../constants/actionTypes';
 import * as api from '../api';
 
 // ACTIONS creators
@@ -10,7 +11,7 @@ export const getPosts = () => async(dispatch) => {
         const {data} = await api.fetchPosts();
 
         //redux will dispatch an action from the data on the backend
-        dispatch({type: 'FETCH_ALL', payload: data});
+        dispatch({type: FETCH_ALL, payload: data});
     } catch (error) {
         console.log(error.message);
     }
@@ -21,7 +22,7 @@ export const createPost = (post)=> async(dispatch)=> {
     try {
         const {data} = await api.createPosts(post);
 
-        dispatch({type: 'CREATE', payload: data});
+        dispatch({type: CREATE, payload: data});
     } catch (error) {
         console.log(error);
     }
@@ -31,7 +32,7 @@ export const updatePost = (id, post) => async (dispatch) => {
     try {
       const { data } = await api.updatePost(id, post);
   
-      dispatch({ type: 'UPDATE', payload: data });
+      dispatch({ type: UPDATE, payload: data });
     } catch (error) {
       console.log(error);
     }
@@ -40,7 +41,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost =(id) => async(dispatch)=> {
     try {
         await api.deletePost(id);
-        dispatch({type: 'DELETE', payload: id});
+        dispatch({type: DELETE, payload: id});
     
     } catch (error) {
         console.log(error);
@@ -51,7 +52,7 @@ export const likePost = (id) => async (dispatch) => {
     try {
         const {data} = await api.likePost(id);
   
-      dispatch({ type: 'UPDATE', payload: data });
+      dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         
     }
