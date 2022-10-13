5937f50afa4375925ec4c302d0ac8c6b204953e3
Hoybaby <mbartek436@gmail.com>
2022-10-13 15:31:27 -0400

    creating the authorization page for when a user needs to see some input fields

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlinedIcon';
+import useStyles from './styles';
 
 const Auth = () => {
+    const classes = useStyles();
     
-    const state = null;
   return (
-    <div>Auth</div>
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon/>
+
+        </Avatar>
+
+      </Paper>
+    </Container>
+
   )
 }
 
